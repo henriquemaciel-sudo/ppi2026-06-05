b3e35d47e17477df823ceb974089f41692ebd7ea
henriquemaciel-sudo <henrique.maciel@escolar.ifrn.edu.br>
2026-05-06 19:19:20 +0000

    Aula do dia 06/05 -introdução a java script

---
+++ ppi26.js
@@ -0,0 +1,41 @@
+// AULA 06/05 - INTRODUÇÃO A JAVA SCRIPT
+
+
+console.log("Bemn vidos a ppi");
+
+// VARIÁVEIS
+
+let name = 'joao' ;
+let age = 17;
+let isStudent = true;
+
+const PI = 3.14; //Variavel que nao pode ser alterada - que ao pode mudar o valor
+console.log(PI);
+
+// OPERADORES ARITIMETICOS + - / * ** %
+let x = 5 + 5; //soma
+let y = '5' + 5 ;  // concatenação
+let z = 'Hello' + 5;
+
+console.log(x**2,y,z );
+console.log(typeof x)
+
+// Ativar o formatador prettier
+// ALT + SHIFT + F]]
+
+
+//OPERADORES RELACIONAIS
+console.log('5' === 5) ;  // compara tipo ou valor 
+console.log('5' === 5);  //comparar tipo e valor 
+
+//OPERADORES DE INCREMENTO 
+
+console.log(x++); // incremeno apos o retorno do valor 
+console.log(x);
+
+console.log(++x);  // incremento antes do retorno do valor 
+
+
+
+
+
